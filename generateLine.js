dbab84018febb4056f7616955b5a6b173657185b
Max Melnychuk <m.melnychyk@skywell.software>
2019-11-18 15:39:39 +0200

    Add new participation param

--- generateLine.js
+++ generateLine.js
@@ -7,10 +7,11 @@ function minMaxRandom(min, max) {
 module.exports = function({ date, now, target, satisfactionRate, importanceRate}) {
   const importance = minMaxRandom(...satisfactionRate);
   const satisfaction = minMaxRandom(...importanceRate);
+  const participation = minMaxRandom(1, 99);
   const ctruScore = Math.round(((importance + satisfaction) / 2) * 10) / 10;
   const time = `to_timestamp('${formatDate(now)}', 'YYYY-MM-DD')`;
 
-  return `insert into history_history (created, updated, date, ctru_score, importance, satisfaction,  target_id) values (${time}, ${time} , '${formatDate(date)}', ${ctruScore}, ${importance} , ${satisfaction}, ${target});`
+  return `insert into history_opinionhistory (created, updated, date, ctru_score, importance, satisfaction,  target_id, participation_share ) values (${time}, ${time} , '${formatDate(date)}', ${ctruScore}, ${importance} , ${satisfaction}, ${target}, ${participation});`
 };
 
 
